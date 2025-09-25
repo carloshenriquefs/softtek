d069dd3644e2b4ff1f28990be706fc72fd93a052
Carlos Henrique <ichfs9108@gmail.com>
2025-09-25 02:31:11 -0300

    feat: add DTO classes and annotations

--- softtek/src/main/java/br/com/softtek/softtek/adapters/inbound/dto/UserRequestDTO.java
+++ softtek/src/main/java/br/com/softtek/softtek/adapters/inbound/dto/UserRequestDTO.java
@@ -1,17 +1,23 @@
 package br.com.softtek.softtek.adapters.inbound.dto;
 
+import br.com.softtek.softtek.domain.user.enums.UserRole;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserRequestDTO(
 
-        @NotBlank(message = "O e-mail do usuário é obrigatória!")
+        @NotBlank(message = "O nome do usuário é obrigatório!")
+        String username,
+
+        @NotBlank(message = "O e-mail do usuário é obrigatório!")
         @Email(message = "O e-mail do usuário não é válido!")
         String email,
 
-        @NotBlank(message = "A senha do usuário é obrigatória!")
+        @NotBlank(message = "A senha do usuário é obrigatório!")
         @Size(min = 6, max = 12, message = "A senha deve conter entre 6 e 12 caracteres!")
-        String password
+        String password,
+
+        UserRole role
 ) {
 }
--- softtek/src/main/java/br/com/softtek/softtek/adapters/inbound/dto/UserResponseDTO.java
+++ softtek/src/main/java/br/com/softtek/softtek/adapters/inbound/dto/UserResponseDTO.java
@@ -1,4 +1,21 @@
 package br.com.softtek.softtek.adapters.inbound.dto;
 
-public class UserResponseDTO {
+import br.com.softtek.softtek.adapters.outbound.jpa.entities.MongoUserEntity;
+import br.com.softtek.softtek.domain.user.enums.UserRole;
+
+public record UserResponseDTO(
+        String id,
+        String username,
+        String email,
+        UserRole role
+) {
+
+    public UserResponseDTO(MongoUserEntity mongoUserEntity) {
+        this(
+                mongoUserEntity.getId(),
+                mongoUserEntity.getUsername(),
+                mongoUserEntity.getEmail(),
+                mongoUserEntity.getRole()
+        );
+    }
 }
